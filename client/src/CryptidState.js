c55337725f7ac05adafee8ef2d230e9c86dd7da0
Gavin <Gavin.rilee@icloud.com>
2021-11-23 10:41:56 -0500

    cryptidlist

---
+++ client/src/CryptidState.js
@@ -0,0 +1,28 @@
+export const CryptidState =  [
+    {
+        title: "50ft Congo Snake",
+        type: "Snake",
+        mainImg: TBD,
+        secondaryImg: TBD,
+        url: "/cryptids/:id",
+        first_sighting: 1959,
+        last_sighting: "Unknown",
+        country: "Democratic Republic of Congo",
+        sighting_location: "Unexplored rainforests of the Congo",
+        source: "https://mysteriesrunsolved.com/2020/05/the-congo-snake.html",
+        description:"In 1959, Remy Van Lierde served as a Colonel in the Belgian Air Force at the Kamina airbase in Belgian occupied Congo. In the Katanga region of the Democratic Republic of the Congo, returning from a mission by helicopter, he reported having seen an enormous snake as he flew over the forests. He described the snake as being close to 50 feet in length, with a 2 foot wide by 3 feet long triangular head, which, if his estimations were accurate, would earn the creature a place amongst the largest snakes to have ever existed. Colonel Van Lierde described the snake as having dark green and brown top scales and a white-ish colored underside. Upon sighting the reptile, he had the pilot turn around and make another pass. at which the serpent reared up the frontal ten feet of its body head as if to strike, giving him the opportunity to observe its underbelly. However, after flying so low that Van Lierde believed it to have been within striking distance of his helicopter, he ordered the pilot to resume his journey, and so the creature was never properly documented, although some reports suggest that an onboard photographer managed to snap a shot of it. It is believed to be either a massively oversized African rock python, a new species of snake entirely, or perhaps a descendant of the giant Eocene snake Gigantophis."  
+    },
+    {
+        title: "Abominable snowman",
+        type: "Bigfoot",
+        mainImg: TBD,
+        secondaryImg: TBD,
+        url: "/cryptids/:id",
+        first_sighting: 1832,
+        last_sighting: "2019",
+        country: "Tibet",
+        sighting_location: "Mountian ranges in variouis countries",
+        source: "https://cryptidz.fandom.com/wiki/Yeti#Sightings",
+        description:"The Yeti, also known as the Abominable Snowman, is an Asian Bigfoot found in the Himalayas. Inhabiting Nepal, Tibet, Russia, and India, the Yeti is probably the closest living relative to Gigantopithecus, a prehistoric ape that lived in Asia three hundred thousand years ago.In Buddhist mythology, the Yetis were peaceful creatures, that were very shy, and lived in dense snow caves on the glaciers which form the Ganges River in India. In modern times, most sightings and evidence are on K2. The name Yeti derives from Yeh-Tah, meaning rock thing.The Yeti was even mentioned in pre-Buddhist cultures, such as the Lecha people, who worshiped a Glacier Giant. Tibetan and Nepalese monasteries have collected several skulls and bones of the yetis, some which preside in the Smithsonian Museum today. Although one of their specimens was proven to be a human bone, others contained the museum are still in question.Yetis according to Twenty-First Century sightings are said to stand from 8 to 10.5 feet tall, have a furry coat of brown, reddish or black hair and to resemble a huge, upright walking ape. Contrary to popular misconception the yeti is not white. The photograph in the gallery below said to be a 'white coated yeti' is in fact a picture of 'Snowflake' the albino gorilla that once lived at Barcelona Zoo. There have been no reports of white yeti in reality. Several expeditions have taken place to find the Yetis, however, only footprints, bones, and hair samples have been  discovered.Yetis have been sighted in the Himalayas since 6th BCE to 2019. It is the oldest recorded Bigfoot, or hairy hominid."  
+    }
+]
